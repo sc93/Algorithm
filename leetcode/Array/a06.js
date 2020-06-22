87cb74f962e8d8a7f56c3d024a62534a6cc43fbd
SC <topsky2008@gmail.com>
2020-06-22 21:29:04 +0900

    a

---
+++ leetcode/Array/a06.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    
+    console.log(nTos1);
+    console.log(nTos2);
+};
+intersect([1,2,2,1],[2,2])
