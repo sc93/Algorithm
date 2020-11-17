e91d9bd121721ecaffd8bfa8419fd5e0af8c5f66
SC <topsky2008@gmail.com>
2020-11-17 17:24:50 +0900

    algo learn

---
+++ programers/lv1/1.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * 정수 배열 numbers가 주어집니다.
+ * numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를
+ * 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+ *
+ */
+
+function solution(numbers) {
+    let answer = '';
+    const set = new Set();
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            set.add(numbers[i] + numbers[j]);
+        }
+    }
+    answer = Array.from(set).sort((a, b) => a - b);
+    return answer;
+}
+
+console.log(solution([5, 0, 2, 7]));
